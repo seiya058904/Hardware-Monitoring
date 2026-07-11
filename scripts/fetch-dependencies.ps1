2bc78c81582ba422eab48237ef4c62f9593cc387
seiya058904 <sunmengsaiyi@gmail.com>
2026-07-11 13:24:14 +0800

    correct pinned PresentMon hash

--- scripts/fetch-dependencies.ps1
+++ scripts/fetch-dependencies.ps1
@@ -4,7 +4,7 @@ $root = Split-Path -Parent $PSScriptRoot
 $tempRoot = Join-Path $env:TEMP "HardwareMonitoringDeps-v2.3.1"
 $presentMonUrl = "https://github.com/GameTechDev/PresentMon/releases/download/v2.3.1/PresentMon-2.3.1-x64.exe"
 $lhmUrl = "https://github.com/LibreHardwareMonitor/LibreHardwareMonitor/releases/download/v0.9.4/LibreHardwareMonitor-net472.zip"
-$presentMonHash = "364E5D98D4D134BD54DD25C22ED2CA2F4883F8BC3ED6502BEE0C151E3436D30"
+$presentMonHash = "364E5D98D4D134BD54DD25C22ED2CA2F4883F8BC3ED6502BEE0C151E3436D30C"
 $lhmZipHash = "D2E397CC4D33D65C6493DFF83B9335BC341A3AF31CAAFCEEF83F717FDAB37448"
 $lhmDllHash = "A0F2728F1734C236A9D02D9E25A88BC4F8CB7BD1FAFF1770726BEB7AF06BF8DC"
 
